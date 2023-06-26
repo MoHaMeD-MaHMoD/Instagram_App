b14a342eb6ff3a591c208dd7bd8383345bef6c7d
MoHaMeD_MaHMoD <Mohamedabdeltawab2212@gmail.com>
2023-06-26 09:56:33 +0300

    fireBase SEtup done For android $ WEB

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_app/Responsive.dart';
 import 'package:instagram_app/mobileScreen.dart';
@@ -7,7 +9,19 @@ import 'package:instagram_app/screens/user/Login.dart';
 import 'package:instagram_app/screens/user/Register.dart';
 import 'package:instagram_app/webScreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+  } else
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBIjpHtCHbFOPICeiETKd2BlXOvq033XAw",
+            authDomain: "instgram-e7c92.firebaseapp.com",
+            projectId: "instgram-e7c92",
+            storageBucket: "instgram-e7c92.appspot.com",
+            messagingSenderId: "582666365592",
+            appId: "1:582666365592:web:18b611c6fbf6dfbb55feed"));
+
   runApp(const MyApp());
 }
 
@@ -17,16 +31,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home:Register()
-      
-      
-      
-      //  Responsive(
-      //   myMobileScreen: MobileScreen(),
-      //   myWebScreen: WebScreen(),
-      // ),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: Register()
+
+        //  Responsive(
+        //   myMobileScreen: MobileScreen(),
+        //   myWebScreen: WebScreen(),
+        // ),
+        );
   }
 }
